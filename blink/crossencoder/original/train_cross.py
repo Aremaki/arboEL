662f503629a04fbd52bffec831d83f5bf46e50a1
Aremaki <ad.remaki@gmail.com>
2025-10-03 00:51:23 +0200

    Add evaluation and save last model

--- blink/crossencoder/original/train_cross.py
+++ blink/crossencoder/original/train_cross.py
@@ -592,6 +592,11 @@ def main(params):
 
     # save the best model in the parent_dir
     logger.info("Best performance in epoch: {}".format(best_epoch_idx))
+    final_output_folder_path = os.path.join(
+        model_output_path, "epoch_{}".format(best_epoch_idx)
+    )
+    utils.save_model(model, tokenizer, final_output_folder_path)
+    logger.info(f"Best model saved at {final_output_folder_path}")
 
 
 if __name__ == "__main__":
